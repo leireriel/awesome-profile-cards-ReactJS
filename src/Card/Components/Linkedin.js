0f45f12ceca6d4f5e888cf0c9c07ef985031700c
leireriel <leirericop@gmail.com>
2019-05-12 16:41:00 +0200

    Cath input value for linkedin

--- src/Card/Components/Linkedin.js
+++ src/Card/Components/Linkedin.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 class Linkedin extends React.Component {
+  HandleInput(event) {
+    const trigger = event.currentTarget.value;
+    console.log(trigger);
+  }
+
   render() {
     return (
       <div className="form__container-linkedin">
         <label
-        className="form_title"
-        for="linkedin">
-        Linkedin *
+          className="form_title"
+          for="linkedin">
+          Linkedin *
         </label>
         <input
           className="form_field"
@@ -16,6 +21,7 @@ class Linkedin extends React.Component {
           id="linkedin"
           placeholder="Ej: linkedin.com/in/sally.hill"
           required
+          onChange={this.HandleInput}
         />
       </div>
     );
