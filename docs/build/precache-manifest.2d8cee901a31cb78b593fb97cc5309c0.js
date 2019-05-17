39507afb8309d8d35e713a778b74d2c33e1500e3
arijdb <ariane.jurado@gmail.com>
2019-05-17 14:22:36 +0200

    pages

---
+++ docs/build/precache-manifest.2d8cee901a31cb78b593fb97cc5309c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ff46fcbd813083b95f025427107553",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb8ec271824087705e71",
+    "url": "./static/css/main.fc8168ec.chunk.css"
+  },
+  {
+    "revision": "f96273690ab938ddcb8c",
+    "url": "./static/js/2.b2c8b6f4.chunk.js"
+  },
+  {
+    "revision": "cb8ec271824087705e71",
+    "url": "./static/js/main.1097dba6.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "73fffbdc7fcf8ba9181c1f8302b40b2e",
+    "url": "./static/media/logo_GameOfCodes.73fffbdc.svg"
+  }
+]);
